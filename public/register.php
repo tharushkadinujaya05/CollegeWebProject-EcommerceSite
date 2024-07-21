2e0777de72893d7407809de9d911c90fbf629835
tharushkadinujaya05 <tharushkadinujaya05@gmail.com>
2024-07-22 02:52:17 +0530

    chore: Update page title in register.php

--- public/register.php
+++ public/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <link href="../assets/css/style.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>IvoryStreets | Login</title>
   </head>
   <body>
     <section class="bg-white">
