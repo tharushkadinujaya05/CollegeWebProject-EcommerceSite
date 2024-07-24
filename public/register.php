7e0ce70e2e80b4b7f8f0ee0b420c05bd4737fca8
tharushkadinujaya05 <tharushkadinujaya05@gmail.com>
2024-07-25 00:50:19 +0530

    Update register page title to "IvoryStreets | SignUp"

--- public/register.php
+++ public/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <link href="../assets/css/style.css" rel="stylesheet" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>IvoryStreets | Login</title>
+    <title>IvoryStreets | SignUp</title>
   </head>
   <body>
     <section class="bg-white">
