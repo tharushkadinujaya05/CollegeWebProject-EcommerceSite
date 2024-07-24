c8048a03f7f5d5601ed220a12e05744a73841955
[Senomi-Jayasinghe] <[jayasinghesr@students.nsbm.ac.lk]>
2024-07-24 22:26:26 +0530

    Create gift.php
    Created Section 1 of Gift Page

---
+++ public/gift.php
@@ -0,0 +1,32 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Gift of Adventure</title>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="flex flex-col items-center bg-white-100 p-6">
+
+        <div class="relative isolate px-6 pt-20 lg:px-8 pb-14">
+        <section class="text-center mb-12 relative isolate px-6 pt-14 lg:px-8">
+            <h1 class="text-5xl font-bold text-gray-800">
+                Give the Gift of <span class="text-red-500">Adventure</span>
+            </h1>
+            <p class="mt-4 text-gray-700 text-lg">
+                We offer curated gift packages designed to inspire and equip <br/>your loved ones for their next 
+                adventure.
+            </p>
+            <a href="#" class="mt-4 inline-block bg-black text-white py-2 px-4 rounded">
+                Button
+            </a>
+        </section>
+        </div>
+        </div>
+    </body>
+</html>
+
+                
+
+            
