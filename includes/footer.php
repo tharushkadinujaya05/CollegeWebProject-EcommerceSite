969a611ae4117c2806fc53243506f2e2d2a9d074
trevonakesh <123149383+trevonakesh@users.noreply.github.com>
2024-07-24 23:04:52 +0530

    Updated Footer
    Adding footer color to the footer

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
-<body>
+<body class="bg-lightGray">
 
   <footer class="bg-lightGray text-gray-700 py-10">
     <div class="container mx-auto px-4">
