5f451552f62f5919634b19bac5d3ebdf0a82c3aa
wskthamel <wskthamel@students.nsbm.ac.lk>
2024-07-24 23:41:04 +0530

    Gift Page Section 2
    Added gift page section 2 (Sinethya)

--- public/gift.php
+++ public/gift.php
@@ -1,4 +1,4 @@
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charSet="UTF-8" />
@@ -23,6 +23,44 @@
             </a>
         </section>
         </div>
+        </div>
+        <div class="flex flex-col items-center bg-white-100 p-6">
+            <main class="flex flex-wrap justify-center gap-8">
+                <div class="flex items-center justify-center min-h-screen bg-white">
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-6xl mx-auto p-6 lg:px-14">
+                        <div class="justify-center pt-14">
+                            <h2 class="text-3xl font-bold mb-2">Black Mamba</h2>
+                            <h3 class="text-xl mb-4">Bold and Enigmatic</h3>
+                            <p class="text-gray-700 mb-6">
+                                 Our Black Mamba packaging is as captivating as its namesake. The inky black exterior with a 
+                                hint of texture exudes an air of sophisticated drama.
+                            </p>
+                            <button class="bg-black text-white py-2 px-4 rounded">Button</button>
+                        </div>
+                        <div class="flex justify-center lg:justify-end">
+                            <img src="../assets/images/gift01.jpg" alt="Black Mamba packaging" class="w-full h-auto max-w-md" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex-col items-center justify-center min-h-screen bg-white">
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 max-w-6xl mx-auto lg:px-8 lg:-my-70">
+                        <div class="flex justify-center lg:justify-start">
+                            <img src="../assets/images/gift02.jpg" alt="White Claw packaging" class="w-full h-auto max-w-md" />
+                        </div>
+                        <div class="justify-center lg:justify-end pl-12 pt-14">
+                            <h2 class="text-3xl font-bold mb-2">White Claw</h2>
+                            <h3 class="text-xl mb-4">Effortlessly Chic Gift Giving</h3>
+                            <p class="text-gray-700 mb-6">
+                                Wrapped in a crisp white exterior, our White Claw gift packaging is the epitome of 
+                                understated elegance. A subtle sheen adds a touch of sophistication, letting the 
+                                perfect gift youve chosen truly shine.
+                            </p>
+                            <button class="bg-black text-white py-2 px-4 rounded">Button</button>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     </body>
 </html>
