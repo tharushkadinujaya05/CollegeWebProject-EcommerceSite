be1697abfc68268fda8a47fd8709e3bafdffff24
tharushkadinujaya05 <tharushkadinujaya05@gmail.com>
2024-07-26 15:03:40 +0530

    feat: Update theme controller to use light mode by default

--- includes/themecontroller.php
+++ includes/themecontroller.php
@@ -1,6 +1,6 @@
 <label class="swap swap-rotate">
   <!-- this hidden checkbox controls the state -->
-  <input type="checkbox" class="theme-controller" value="synthwave" />
+  <input type="checkbox" class="theme-controller" value="light" />
 
   <!-- sun icon -->
   <svg
