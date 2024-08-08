176ddc7afc4c0521144facbee975006a721833f4
Savishka Dilshan <lsdliyanage@students.nsbm.ac.lk>
2024-08-08 10:46:15 +0530

    Update the index.php

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');</style>
+</head>
+<body>
+
+  
+  <!-- HERO1 -->
+  <div class="relative h-screen">
+    <!-- Background Image -->
+    <div class="hidden md:block absolute inset-0 bg-cover bg-center" style="background-image: url('home01_desktop.jpg');"></div>
+    <div class="md:hidden absolute inset-0 bg-cover bg-center" style="background-image: url('home01_mobile.jpg');"></div>
+    <!-- Hero Text + CTA -->
+    <div class="absolute inset-0 flex flex-col justify-center items-start px-8 py-12 md:px-16 lg:px-24">
+
+      <!-- Desktop -->
+      <h1 class="text-2xl md:text-4xl lg:text-6xl font-bold text-black mb-4 font-serif hidden sm:block">
+        The<br>Statement<br>Maker</h1>
+      <h2 class="text-lg md:text-xl lg:text-2xl text-gray-700 mb-8 hidden sm:block" style="font-family: 'Inter', sans-serif; font-weight: 500;">
+        Refresh Your Look,<br>20% Off Graphic Tees!</h2>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full focus:outline-none focus:shadow-outline hidden sm:block">
+        Shop Now
+      </button>
+
+      <!-- Mobile -->
+    </div>
+  </div>
+
+
+  <!-- HERO2 -->
+  
+</body>
+</html>
