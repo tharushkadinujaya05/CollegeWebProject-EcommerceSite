e5d7dc2ad028e927cfffb80a094dc40d841a1c83
Tharushka Dinujaya <tharushkadinujaya05@gmail.com>
2024-08-16 02:13:36 +0530

    refactor: Update image paths and font styles in index.php

--- public/blog/index.php
+++ public/blog/index.php
@@ -3,9 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Explore Page - Ivory Streets</title>
-    <link rel="stylesheet" href="style.css">
+    <link href="../../assets/css/style.css" rel="stylesheet" />
     <link rel="shortcut icon" href="images/favicon-og.png" type="image/x-icon">
-    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         .hidden-content{
             display: none;
@@ -13,7 +12,7 @@
     </style>
 </head>
 <body>
-    <div class="relative bg-cover bg-center h-[36rem] md:h-[40rem]" style="background-image: url('images/blog01.png');">
+    <div class="relative bg-cover bg-center h-[36rem] md:h-[40rem]" style="background-image: url('../../assets/images/blog01.png');">
         <div class="absolute inset-0 bg-black bg-opacity-50"></div>
         <div class="absolute inset-0 flex flex-col items-center justify-center px-4">
             <p class="text-white text-3xl md:text-5xl font-semibold text-center mb-4">
@@ -25,11 +24,11 @@
         </div>
     </div> 
 
-    <div class="text-black text-2xl md:text-3xl font-normal ml-4 md:ml-12 mt-8 mb-6">Latest Blogs</div>
-    <div class="ml-4 md:ml-12 mr-4 md:mr-12 relative bg-cover bg-center h-[24rem] md:h-[30rem] rounded" style="background-image: url('images/blog02.png');">
+    <div class="text-black text-2xl md:text-3xl font-poppins ml-4 md:ml-12 mt-8 mb-6">Latest Blogs</div>
+    <div class="ml-4 md:ml-12 mr-4 md:mr-12 relative bg-cover bg-center h-[24rem] md:h-[30rem] rounded" style="background-image: url('../../assets/images/blog02.png');">
         <div class="absolute inset-0 flex items-center justify-center">
             <div class="bg-white w-full md:w-[40rem] h-auto md:h-[21.5rem] backdrop-blur-lg p-4 rounded flex flex-col justify-center">
-                <div class="ml-4 md:ml-8 mr-4 md:mr-8 mt-6 md:mt-2 mb-3 text-sm md:text-xl text-justify">
+                <div class="ml-4 md:ml-8 mr-4 md:mr-8 mt-6 md:mt-2 mb-3 text-sm md:text-xl text-justify font-nunito">
                     Summer is here, and it's time to refresh your wardrobe with the latest trends. At <span class="font-bold">Ivory Streets</span>, we are dedicated to helping you stay ahead of the fashion curve. This season, we are bringing you a blend of classic styles and contemporary twists that will keep you looking chic and feeling comfortable. Here are the top five summer fashion trends you need to embrace with our latest collection.
                 </div>
                 <span class="text-right mr-4 md:mr-8 text-gray-500 font-semibold text-sm md:text-base">8<sup>th</sup> June 2024</span>
@@ -47,7 +46,7 @@
   <div class="flex flex-wrap -mx-2 md:-mx-4 mx-4 md:mx-4 mt-16  "> 
     <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-2 md:px-4 mb-6">
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="images/blog04.jpg" alt="Blog post 1 image">
+            <img class="w-full" src="'../../assets/images/blog04.jpg" alt="Blog post 1 image">
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Music & Streetwear</div>
                 <a href="Music and Streetwear.html">
@@ -60,7 +59,7 @@
 
     <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-2 md:px-4 mb-6">
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="images/blog05.jpg" alt="Blog post 2 image">
+            <img class="w-full" src="../../assets/images/blog05.jpg" alt="Blog post 2 image">
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">The Fabric of Exploration</div>
                 <a href="The Fabric of Exploration.html">
@@ -73,7 +72,7 @@
 
     <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-2 md:px-4 mb-6">
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="images/blog06.jpg" alt="Blog post 3 image">
+            <img class="w-full" src="../../assets/images/blog06.jpg" alt="Blog post 3 image">
             <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Discover more with Ivory Streets</div>
                 <a href="Discover More with Ivory Streets.html">
