ee2663ccebdb168807f772a6f6fffa02cde13323
Tharushka Dinujaya <tharushkadinujaya05@gmail.com>
2024-08-16 04:44:05 +0530

    refactor: Update FAQ page layout and content

--- public/faq/includes/deliveryandorder.php
+++ public/faq/includes/deliveryandorder.php
@@ -1,61 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="../../assets/css/style.css" rel="stylesheet" />
-    <title>IvoryStreets | FAQ</title>
-  </head>
-  <body>
-    <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-      <h2
-        class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white"
-      >
-        Frequently<br />asked questions
-      </h2>
-      <p class="mt-1 hidden md:block text-gray-600 dark:text-neutral-400">
-        Answers to the most frequently asked questions.
-      </p>
-
-      <div class="grid md:grid-cols-5 gap-10 pt-10">
-        <div class="md:col-span-2">
-          <div class="max-w-xs">
-            <h1
-              class="text-2xl font-poppins md:text-2xl md:leading-tight dark:text-white"
-            >
-              Catergories
-            </h1>
-            <div class="mt-4 space-y-4 lg:mt-8">
-              <a
-                href="#"
-                class="block text-blue-500 dark:text-blue-400 hover:underline"
-                >Delivery and order tracking
-              </a>
-              <a
-                href="#"
-                class="block text-gray-500 dark:text-gray-300 hover:underline"
-                >Payments and promotions
-              </a>
-              <a
-                href="#"
-                class="block text-gray-500 dark:text-gray-300 hover:underline"
-                >Returns, exchanges and refunds
-              </a>
-              <a
-                href="#"
-                class="block text-gray-500 dark:text-gray-300 hover:underline"
-                >Information on sizes and products
-              </a>
-              <a
-                href="#"
-                class="block text-gray-500 dark:text-gray-300 hover:underline"
-                >Personal data and notifications</a
-              >
-            </div>
-          </div>
-        </div>
-
-        <div class="md:col-span-3">
+<div class="md:col-span-3">
           <div
             class="hs-accordion-group divide-y divide-gray-200 dark:divide-neutral-700"
           >
@@ -391,8 +334,3 @@
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <script src="../../node_modules/preline/dist/preline.js"></script>
-  </body>
-</html>
