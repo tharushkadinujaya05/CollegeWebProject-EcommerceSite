65d36634cd02cd11cece96c1e79521675a42e2fd
Tharushka Dinujaya <tharushkadinujaya05@gmail.com>
2024-08-24 01:21:21 +0530

    feat: added filter options in Ready to wear page

--- public/readytowear.php
+++ public/readytowear.php
@@ -25,55 +25,210 @@
       </p>
     </header>
 
-    <div class="mt-8">
-      <p class="text-sm text-gray-500">Showing <span> 1 </span> of 60</p>
+    <div class="mt-8 block lg:hidden">
+      <button
+        class="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
+      >
+        <span class="text-sm font-medium"> Filters & Sorting </span>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-4 rtl:rotate-180"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+      </button>
     </div>
 
-    <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-      <li>
-        <a href="#" class="group block overflow-hidden">
-          <img
-            src="../assets/images/model.png"
-            alt=""
-            class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-          />
+    <div class="mt-4 lg:mt-8 lg:grid lg:grid-cols-4 lg:items-start lg:gap-8">
+      <div class="hidden space-y-4 lg:block">
+        <div>
+          <label for="SortBy" class="block text-xs font-medium text-gray-700"> Sort By </label>
 
-          <div class="relative bg-white pt-3">
-            <h3 class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-            OVERSIZED DRAGON FLORAL GRAPHIC SWEATSHIRT
-            </h3>
+          <select id="SortBy" class="mt-1 rounded border-gray-300 text-sm">
+            <option>Sort By</option>
+            <option value="Title, DESC">Price, Low to High</option>
+            <option value="Title, ASC">Price,, High to Low</option>
+          </select>
+        </div>
 
-            <p class="mt-2">
-              <span class="sr-only"> Regular Price </span>
+        <div>
+          <p class="block text-xs font-medium text-gray-700">Filters</p>
 
-              <span class="tracking-wider text-gray-900"> $19.60 </span>
-            </p>
+          <div class="mt-1 space-y-2">
+            <details
+              class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
+            >
+              <summary
+                class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition"
+              >
+                <span class="text-sm font-medium"> Availability </span>
+
+                <span class="transition group-open:-rotate-180">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </span>
+              </summary>
+
+              <div class="border-t border-gray-200 bg-white">
+                <header class="flex items-center justify-between p-4">
+                  <span class="text-sm text-gray-700"> 0 Selected </span>
+
+                  <button type="button" class="text-sm text-gray-900 underline underline-offset-4">
+                    Reset
+                  </button>
+                </header>
+
+                <ul class="space-y-1 border-t border-gray-200 p-4">
+                  <li>
+                    <label for="FilterInStock" class="inline-flex items-center gap-2">
+                      <input
+                        type="checkbox"
+                        id="FilterInStock"
+                        class="size-5 rounded border-gray-300"
+                      />
+
+                      <span class="text-sm font-medium text-gray-700"> In Stock (5+) </span>
+                    </label>
+                  </li>
+
+                  <li>
+                    <label for="FilterPreOrder" class="inline-flex items-center gap-2">
+                      <input
+                        type="checkbox"
+                        id="FilterPreOrder"
+                        class="size-5 rounded border-gray-300"
+                      />
+
+                      <span class="text-sm font-medium text-gray-700"> Pre Order (3+) </span>
+                    </label>
+                  </li>
+
+                  <li>
+                    <label for="FilterOutOfStock" class="inline-flex items-center gap-2">
+                      <input
+                        type="checkbox"
+                        id="FilterOutOfStock"
+                        class="size-5 rounded border-gray-300"
+                      />
+
+                      <span class="text-sm font-medium text-gray-700"> Out of Stock (10+) </span>
+                    </label>
+                  </li>
+                </ul>
+              </div>
+            </details>
+
+            <details
+              class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
+            >
+              <summary
+                class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition"
+              >
+                <span class="text-sm font-medium"> Price </span>
+
+                <span class="transition group-open:-rotate-180">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                    />
+                  </svg>
+                </span>
+              </summary>
+
+              <div class="border-t border-gray-200 bg-white">
+                <header class="flex items-center justify-between p-4">
+                  <span class="text-sm text-gray-700"> The highest price is $600 </span>
+
+                  <button type="button" class="text-sm text-gray-900 underline underline-offset-4">
+                    Reset
+                  </button>
+                </header>
+
+                <div class="border-t border-gray-200 p-4">
+                  <div class="flex justify-between gap-4">
+                    <label for="FilterPriceFrom" class="flex items-center gap-2">
+                      <span class="text-sm text-gray-600">$</span>
+
+                      <input
+                        type="number"
+                        id="FilterPriceFrom"
+                        placeholder="From"
+                        class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                      />
+                    </label>
+
+                    <label for="FilterPriceTo" class="flex items-center gap-2">
+                      <span class="text-sm text-gray-600">$</span>
+
+                      <input
+                        type="number"
+                        id="FilterPriceTo"
+                        placeholder="To"
+                        class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                      />
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </details>
+
+            
           </div>
-        </a>
-      </li>
-    </ul>
+        </div>
+      </div>
 
-    <ol class="mt-8 flex justify-center gap-1 text-xs font-medium">
-      <li>
-        <a
-          href="#"
-          class="inline-flex size-8 items-center justify-center rounded border border-gray-100"
-        >
-          <span class="sr-only">Prev Page</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="size-3"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </a>
-      </li>
+      <div class="lg:col-span-3">
+        <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          <li>
+            <a href="#" class="group block overflow-hidden">
+              <img
+                src="../assets/images/model.png"
+                alt=""
+                class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+              />
+
+              <div class="relative bg-white pt-3">
+                <h3
+                  class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+                >
+                OVERSIZED DRAGON FLORAL GRAPHIC SWEATSHIRT
+                </h3>
+
+                <p class="mt-2">
+                  <span class="tracking-wider text-gray-900">$19.75</span>
+                </p>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <ol class="mt-8 flex justify-center gap-1 text-xs font-medium">
+     
 
       <li>
         <a href="#" class="block size-8 rounded border border-gray-100 text-center leading-8">
@@ -116,6 +271,8 @@
         </a>
       </li>
     </ol>
+      </div>
+    </div>
   </div>
 </section>
 </body>
