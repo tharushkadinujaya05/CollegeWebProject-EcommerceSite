1e4a38882c48f78214e94930305c4d1ed85f3737
trevonakesh <123149383+trevonakesh@users.noreply.github.com>
2024-08-23 20:39:47 +0530

    Bug: Fix Footer Logo Alignment on Screens Smaller Than 768px

--- includes/footer.php
+++ includes/footer.php
@@ -1,84 +1,79 @@
-<!-- include/footer.php -->
-<footer class="bg-gray-200 text-gray-700 py-10 block lg:hidden">
-  <div class="container mx-auto px-4">
-    <div class="grid grid-cols-1 md:grid-cols-6 gap-8">
-      
-      <div>
-        <h5 class="font-bold mb-4">EXPLORE</h5>
-        <ul>
-          <li class="mb-2"><a href="#" class="hover:underline">New In</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Ready to Wear</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Gifts</a></li>
-        </ul>
+
+
+  <footer class="bg-gray-200 text-gray-700 py-10">
+    <div class="container mx-auto px-4">
+      <div class="grid grid-cols-1 md:grid-cols-6 gap-8">
+        
+        <div>
+          <h5 class="font-bold mb-4">EXPLORE</h5>
+          <ul>
+            <li class="mb-2"><a href="#" class="hover:underline">New In</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Ready to Wear</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Gifts</a></li>
+          </ul>
+        </div>
+        
+        <div>
+          <h5 class="font-bold mb-4">Ready to Buy</h5>
+          <ul>
+            <li class="mb-2"><a href="#" class="hover:underline">T-shirts and Vests</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Hoodies and Sweaters</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Jeans</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Shirts</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Shorts</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Cargos</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Coats & Jackets</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Graphic Tees</a></li>
+          </ul>
+        </div>
+        
+        <div>
+          <h5 class="font-bold mb-4">ABOUT US</h5>
+          <ul>
+            <li class="mb-2"><a href="#" class="hover:underline">About IVORY STREETS</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Get to Know the Team</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Terms and Conditions</a></li>
+          </ul>
+        </div>
+        
+        <div>
+          <h5 class="font-bold mb-4">Contact Us</h5>
+          <ul>
+            <li class="mb-2"><a href="#" class="hover:underline">Terms of Use</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Returns</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Privacy Policy</a></li>
+            <li class="mb-2"><a href="#" class="hover:underline">Cookie Contents</a></li>
+          </ul>
+        </div>
+        
+        <div class="md:col-span-2 mx-10 mt-20"> 
+          <h5 class="font-bold mb-4">Get the latest news from us</h5>
+          <form class="mb-4">
+            <input type="email" placeholder="Enter your email address" class="w-full px-3 py-2 mb-2 border border-gray-300 rounded">
+            <button type="submit" class="w-full bg-black text-white py-2 rounded">Subscribe</button>
+          </form>
+          <p class="text-sm">By signing up, you agree to our <a href="#" class="underline">Privacy Policy</a> and <a href="#" class="underline">Terms of Service</a>.</p>
+        </div>
+        
       </div>
       
-      <div class="mt-10 flex flex-col md:flex-row justify-start md:justify-between items-start md:items-center mx-10">
+      <div class="mt-10 flex flex-col md:flex-row md:justify-between md:items-center mx-10 ">
         <p class="text-sm" id="year">
-            <img class="h-5" src="../assets/images/logo-bl.png"><br>
-            &copy;2024 IVORYSTREETS All rights reserved
-        </p>
+          <img src="assets\images\logo-bl.png" class="w-130 h-70"><br>
+          &copy;2024 IVORY STREETS All rights reserved</p>
         <div class="flex space-x-4 mt-4 md:mt-0">
           <a href="https://www.facebook.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-facebook fa-lg"></i></a>
           <a href="https://www.twitter.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-twitter fa-lg"></i></a>
           <a href="https://www.instagram.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-instagram fa-lg"></i></a>
           <a href="https://www.linkedin.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-linkedin fa-lg"></i></a>
         </div>
-    </div>
-      <div>
-        <h5 class="font-bold mb-4">READY TO BUY</h5>
-        <ul>
-          <li class="mb-2"><a href="#" class="hover:underline">T-shirts and Vests</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Hoodies and Sweaters</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Jeans</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Shirts</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Shorts</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Cargos</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Coats & Jackets</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Graphic Tees</a></li>
-        </ul>
-      </div>
-      
-      <div>
-        <h5 class="font-bold mb-4">ABOUT US</h5>
-        <ul>
-          <li class="mb-2"><a href="#" class="hover:underline">About IVORY STREETS</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Get to Know the Team</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Terms and Conditions</a></li>
-        </ul>
-      </div>
-      
-      <div>
-        <h5 class="font-bold mb-4">CONTACT US</h5>
-        <ul>
-          <li class="mb-2"><a href="#" class="hover:underline">Terms of Use</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Returns</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Privacy Policy</a></li>
-          <li class="mb-2"><a href="#" class="hover:underline">Cookie Contents</a></li>
-        </ul>
-      </div>
-      
-      <div class="md:col-span-2 mx-10 mt-20"> 
-        <h5 class="font-bold mb-4">Get the latest news from us</h5>
-        <form class="mb-4">
-          <input type="email" placeholder="Enter your email address" class="w-full px-3 py-2 mb-2 border border-gray-300 rounded">
-          <button type="submit" class="w-full bg-black text-white py-2 rounded">Subscribe</button>
-        </form>
-        <p class="text-sm">By signing up, you agree to our <a href="#" class="underline">Privacy Policy</a> and <a href="#" class="underline">Terms of Service</a>.</p>
       </div>
       
     </div>
-    
-    <div class="mt-10 flex flex-col md:flex-row md:justify-between md:items-center">
-      <p class="text-sm" id="year">
-          <img class="h-5" src="../assets/images/logo-bl.png" ><br>
-          &copy;2024 IVORYSTREETS All rights reserved</p> 
-      <div class="flex space-x-4 mt-4 md:mt-0 mx-10">
-        <a href="https://www.facebook.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-facebook fa-lg"></i></a>
-        <a href="https://www.twitter.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-twitter fa-lg"></i></a>
-        <a href="https://www.instagram.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-instagram fa-lg"></i></a>
-        <a href="https://www.linkedin.com" class="text-gray-500 hover:text-gray-800"><i class="fab fa-linkedin fa-lg"></i></a>
-      </div>
-    </div>
-    n
-  </div>
-</footer>
+  </footer>
+
+ 
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous">
+    document.getElementById('year').textContent = new Date().getFullYear();
+  </script>
+
