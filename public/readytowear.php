78eafd72f58fab08a9eb35ca41fdaaaa9f104b20
[Senomi-Jayasinghe] <[jayasinghesr@students.nsbm.ac.lk]>
2024-09-02 01:26:55 +0530

    Update readytowear.php
    Updated ready to wear page. Implemented filter and Select query. Senomi Jayasinghe

--- public/readytowear.php
+++ public/readytowear.php
@@ -31,6 +31,9 @@
     if (in_array('InStock', $availability)) {
         $availabilityConditions[] = "product_isactive = 'A'";
     }
+    if (in_array('PreOrder', $availability)) {
+        $availabilityConditions[] = "product_isactive = 'P'";
+    }
     if (in_array('OutOfStock', $availability)) {
         $availabilityConditions[] = "product_isactive = 'I'";
     }
@@ -86,171 +89,83 @@
 
     <div class="mt-4 lg:mt-8 lg:grid lg:grid-cols-4 lg:items-start lg:gap-8">
       <div class="hidden space-y-4 lg:block">
+      <!--DID CHANGES-->
       <form method="GET" action="">
         <div>
           <label for="SortBy" class="block text-xs font-medium text-gray-700"> Sort By </label>
-
           <select id="SortBy" name="sortBy" class="mt-1 rounded border-gray-300 text-sm">
-            <option>Sort By</option>
-            <option value="ASC">Price, Low to High</option>
-            <option value="DESC">Price, High to Low</option>
+            <option value="ASC" <?= $sortBy == 'ASC' ? 'selected' : '' ?>>Price, Low to High</option>
+            <option value="DESC" <?= $sortBy == 'DESC' ? 'selected' : '' ?>>Price, High to Low</option>
           </select>
         </div>
 
         <div>
           <p class="block text-xs font-medium text-gray-700">Filters</p>
-
           <div class="mt-1 space-y-2">
-            <details
-              class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
-            >
-              <summary
-                class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition"
-              >
-                <span class="text-sm font-medium"> Availability </span>
-
-                <span class="transition group-open:-rotate-180">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="size-4"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                    />
-                  </svg>
-                </span>
-              </summary>
-
-              <div class="border-t border-gray-200 bg-white">
-                <header class="flex items-center justify-between p-4">
-                  <span class="text-sm text-gray-700"> 0 Selected </span>
-
-                  <button type="button" class="text-sm text-gray-900 underline underline-offset-4">
-                    Reset
-                  </button>
-                </header>
-
-                <ul class="space-y-1 border-t border-gray-200 p-4">
-                  <li>
-                    <label for="FilterInStock" class="inline-flex items-center gap-2">
-                      <input
-                        type="checkbox"
-                        id="FilterInStock"
-                        class="size-5 rounded border-gray-300"
-                        name="availability[]"
-                        value="InStock"
-                      />
-
-                      <span class="text-sm font-medium text-gray-700"> In Stock (5+) </span>
-                    </label>
-                  </li>
-
-                  <li>
-                    <label for="FilterPreOrder" class="inline-flex items-center gap-2">
-                      <input
-                          type="checkbox"
-                          id="FilterPreOrder"
-                          class="size-5 rounded border-gray-300"
-                          name="availability[]"
-                          value="PreOrder"
-                      />
-
-                      <span class="text-sm font-medium text-gray-700"> Pre Order (3+) </span>
-                    </label>
-                  </li>
-
-                  <li>
-                    <label for="FilterOutOfStock" class="inline-flex items-center gap-2">
-                      <input
-                          type="checkbox"
-                          id="FilterOutOfStock"
-                          class="size-5 rounded border-gray-300"
-                          name="availability[]"
-                          value="OutOfStock
-                      />
-
-                      <span class="text-sm font-medium text-gray-700"> Out of Stock (10+) </span>
-                    </label>
-                  </li>
-                </ul>
-              </div>
-            </details>
-
-            <details
-              class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
-            >
-              <summary
-                class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition"
-              >
-                <span class="text-sm font-medium"> Price </span>
-
-                <span class="transition group-open:-rotate-180">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="size-4"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                    />
-                  </svg>
-                </span>
-              </summary>
-
-              <div class="border-t border-gray-200 bg-white">
-                <header class="flex items-center justify-between p-4">
-                  <span class="text-sm text-gray-700"> The highest price is $600 </span>
-
-                  <button type="button" class="text-sm text-gray-900 underline underline-offset-4">
-                    Reset
-                  </button>
-                </header>
-
-                <div class="border-t border-gray-200 p-4">
-                  <div class="flex justify-between gap-4">
-                    <label for="FilterPriceFrom" class="flex items-center gap-2">
-                      <span class="text-sm text-gray-600">$</span>
-
-                      <input
-                          type="number"
-                          id="FilterPriceFrom"
-                          placeholder="From"
-                          class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                          name="From"
-                      />
-                    </label>
-
-                    <label for="FilterPriceTo" class="flex items-center gap-2">
-                      <span class="text-sm text-gray-600">$</span>
-
-                      <input
-                          type="number"
-                          id="FilterPriceTo"
-                          placeholder="To"
-                          class="w-full w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                      />
-                    </label>
-                  </div>
+            <details class="overflow-hidden rounded border border-gray-300">
+                <summary class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
+                    <span class="text-sm font-medium"> Availability </span>
+                    <span class="transition group-open:-rotate-180">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </span>
+                </summary>
+                <div class="border-t border-gray-200 bg-white">
+                    <ul class="space-y-1 border-t border-gray-200 p-4">
+                        <li>
+                            <label for="FilterInStock" class="inline-flex items-center gap-2">
+                                <input type="checkbox" id="FilterInStock" class="size-5 rounded border-gray-300" name="availability[]" value="InStock" <?= in_array('InStock', $availability) ? 'checked' : '' ?> />
+                                <span class="text-sm font-medium text-gray-700"> In Stock (5+) </span>
+                            </label>
+                        </li>
+                        <li>
+                            <label for="FilterPreOrder" class="inline-flex items-center gap-2">
+                                <input type="checkbox" id="FilterPreOrder" class="size-5 rounded border-gray-300" name="availability[]" value="PreOrder" <?= in_array('PreOrder', $availability) ? 'checked' : '' ?> />
+                                <span class="text-sm font-medium text-gray-700"> Pre Order (3+) </span>
+                            </label>
+                        </li>
+                        <li>
+                            <label for="FilterOutOfStock" class="inline-flex items-center gap-2">
+                                <input type="checkbox" id="FilterOutOfStock" class="size-5 rounded border-gray-300" name="availability[]" value="OutOfStock" <?= in_array('OutOfStock', $availability) ? 'checked' : '' ?> />
+                                <span class="text-sm font-medium text-gray-700"> Out of Stock (10+) </span>
+                            </label>
+                        </li>
+                    </ul>
                 </div>
-              </div>
             </details>
 
-            
-          </div>
-        
+            <details class="overflow-hidden rounded border border-gray-300">
+                <summary class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
+                    <span class="text-sm font-medium"> Price </span>
+                    <span class="transition group-open:-rotate-180">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </span>
+                </summary>
+                <div class="border-t border-gray-200 bg-white">
+                    <div class="border-t border-gray-200 p-4">
+                        <div class="flex justify-between gap-4">
+                            <label for="FilterPriceFrom" class="flex items-center gap-2">
+                                <span class="text-sm text-gray-600">$</span>
+                                <input type="number" id="FilterPriceFrom" placeholder="From" class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm" name="From" value="<?= $priceFrom ?>" />
+                            </label>
+                            <label for="FilterPriceTo" class="flex items-center gap-2">
+                                <span class="text-sm text-gray-600">$</span>
+                                <input type="number" id="FilterPriceTo" placeholder="To" class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm" name="To" value="<?= $priceTo ?>" />
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </details>
         </div>
-        </form>
+    </div>
+    <button type="submit" class="mt-4 inline-flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-800">
+      Apply Filters
+    </button>
+    </form>
+    <!--CHANGES END-->
       </div>
 
       <div class="lg:col-span-3">
@@ -259,7 +174,7 @@
             while ($row = mysqli_fetch_assoc($result)) {
               echo '<li>';
               echo '<a href="#" class="group block overflow-hidden">';
-              echo '<img src="' . htmlspecialchars($row['product_image']) . '" alt="" class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]" />';
+              echo '<img src="data:image/png;charset=utf8;base64,' . base64_encode($row['product_image']) . '" class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"/>';
               echo '<div class="relative bg-white pt-3">';
               echo '<h3 class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">' . htmlspecialchars($row['product_name']) . '</h3>';
               echo '<p class="mt-2"><span class="tracking-wider text-gray-900">$' . htmlspecialchars($row['product_price']) . '</span></p>';
