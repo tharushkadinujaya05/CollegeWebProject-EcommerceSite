eea1140876042cc3189a663cc65a6e81a12e4973
[Senomi-Jayasinghe] <[jayasinghesr@students.nsbm.ac.lk]>
2024-09-03 23:36:05 +0530

    chore: changes css path location

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IvoryStreets | Home</title>
-    <link rel="stylesheet" href="../assets/css/style.css" />
-    <link rel="stylesheet" href="../assets/css/nav_cart.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/nav_cart.css" />
     </style>
     </head>
     <body>
