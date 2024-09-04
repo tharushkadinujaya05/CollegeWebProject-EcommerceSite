e1ae815d0b0b03082ba8e21d6964bb1ae7ed5098
[Senomi-Jayasinghe] <[jayasinghesr@students.nsbm.ac.lk]>
2024-09-05 00:34:35 +0530

    Update cart.php
    Fixed link to readytowear.php

--- public/includes/cart.php
+++ public/includes/cart.php
@@ -113,7 +113,7 @@
                 <a href="public/checkout.php?order_id=<?php echo urlencode($order_id); ?>&subtotal=<?php echo urlencode($subtotal); ?>" class="flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue">Checkout</a>
               </div>
               <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
-                <p>or <button type="button" class="font-medium text-blue-600 hover:text-blue-500">Continue Shopping <span aria-hidden="true"> &rarr;</span></button></p>
+                <p>or <a href="public/readytowear.php"><button type="button" class="font-medium text-blue-600 hover:text-blue-500">Continue Shopping <span aria-hidden="true"> &rarr;</span></button></a></p>
               </div>
             </div>
           </div>
